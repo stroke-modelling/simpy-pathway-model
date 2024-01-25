9c58d536cb4513554784664222960f56bd1feefe
Anna Laws <a.laws2@exeter.ac.uk>
2024-01-25 09:17:50 +0000

    Moved output dir routine to function

--- classes/units.py
+++ classes/units.py
@@ -35,44 +35,9 @@ class Units(object):
         # Set up paths to files.
         dir_output_all_runs = './output/'
         # Try this name for an output folder:
-        dir_output = 'run'
-        # Choose a delimiter that wouldn't normally go into a dir
-        # name just to make the naming easier.
-        delim = '!'
-        # Check if output folder already exists:
-        dir_output_this_run = os.path.join(dir_output_all_runs, dir_output)
-        while os.path.isdir(dir_output_this_run):
-            if ((dir_output[-1] == '/') | (dir_output[-1] == '\\')):
-                # Remove final '/' or '\'
-                dir_output = dir_output[:-1]
-            # Split the dir name by every delim:
-            dir_parts = dir_output.split(delim)
-            # # Make a single string of everything up to the final delim:
-            # For now, assume that the delimiter doesn't appear
-            # elsewhere in the file name.
-            dir_start = dir_parts[0]
-            # dir_start = (
-            #     delim.join(dir_parts.split(delim)[:-1])
-            #     if len(dir_parts) > 1 else dir_output)
-            if len(dir_parts) == 1:
-                # No delim in it yet. Set up the suffix.
-                suffix = 1
-            else:
-                # Increase the number after the delim.
-                suffix = dir_parts[-1]
-                try:
-                    suffix = int(suffix)
-                    suffix += 1
-                except ValueError:
-                    # The final part of the directory name is not
-                    # a number.
-                    suffix = 1
-            # Update the directory name:
-            dir_output = f'{dir_start}{delim}{suffix}'
-            dir_output_this_run = os.path.join(dir_output_all_runs, dir_output)
-        # Create this directory:
-        os.mkdir(dir_output_this_run)
-        # TO DO - Windows uses the other type of slash, change this to pathlib or another way of accounting for this.
+        dir_output = 'run'  # Don't put a slash at the end please.
+        dir_output_this_run = self._create_output_dir(
+            dir_output_all_runs, dir_output)
 
         # make a new output folder for each run.
         self.paths_dict = dict(
@@ -156,6 +121,70 @@ class Units(object):
         )
 
         return national_dict
+
+    def _create_output_dir(dir_output_all_runs, dir_output, delim='!'):
+        """
+        Create a directory for storing the output of this run.
+
+        If the chosen directory name already exists, add a suffix
+        "!1" or similar to make a new directory name and create that
+        instead.
+
+        Choose a delimiter that wouldn't normally go into a dir
+        name just to make the naming easier.
+
+        Inputs
+        ------
+        dir_output_all_runs - str. Name of the main output directory
+                              for all runs.
+        dir_output          - str. Requested name of the output directory
+                              for this run of the model only.
+        delim               - str. A character to split off the requested
+                              directory name from the suffix that this
+                              function adds to it.
+        """
+        # Check if output folder already exists:
+        dir_output_this_run = os.path.join(dir_output_all_runs, dir_output)
+
+        # While the requested output folder already exists:
+        while os.path.isdir(dir_output_this_run):
+            if ((dir_output[-1] == '/') | (dir_output[-1] == '\\')):
+                # Remove final '/' or '\'
+                dir_output = dir_output[:-1]
+            # Split the dir name by every delim:
+            dir_parts = dir_output.split(delim)
+            # # Make a single string of everything up to the final delim:
+            # For now, assume that the delimiter doesn't appear
+            # elsewhere in the file name.
+            dir_start = dir_parts[0]
+            # If delimiter would appear elsewhere, would need something
+            # like this only need a way to tell the difference between
+            # a name ending _1 and ending _useful_stuff.
+            # dir_start = (
+            #     delim.join(dir_parts.split(delim)[:-1])
+            #     if len(dir_parts) > 1 else dir_output)
+            if len(dir_parts) == 1:
+                # No delim in it yet. Set up the suffix.
+                suffix = 1
+            else:
+                # Increase the number after the delim.
+                suffix = dir_parts[-1]
+                try:
+                    suffix = int(suffix)
+                    suffix += 1
+                except ValueError:
+                    # The final part of the directory name is not
+                    # a number.
+                    suffix = 1
+            # Update the directory name:
+            dir_output = f'{dir_start}{delim}{suffix}'
+            dir_output_this_run = os.path.join(
+                dir_output_all_runs, dir_output)
+        # Create this directory:
+        os.mkdir(dir_output_this_run)
+        # Return the name so that we can point the code
+        # at this directory:
+        return dir_output_this_run
 
     # ################################
     # ##### NATIONAL INFORMATION #####
